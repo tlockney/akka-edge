0a9b5093b76de6b18cadfd0e4d5d7503436854c9
Raymond Tay <raymondtay1974@gmail.com>
2013-09-24 21:34:41 +0800

    multi-node-testing

--- ch9-testing-actors/build.sbt
+++ ch9-testing-actors/build.sbt
@@ -5,10 +5,9 @@ version := "1.0"
 scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
-  "org.eclipse.jetty" % "jetty-server" % "9.0.0.v20130308",
-  "org.eclipse.jetty" % "jetty-webapp" % "9.0.0.v20130308",
   "com.typesafe.akka" %% "akka-actor" % "2.2.1",
   "com.typesafe.akka" %% "akka-testkit" % "2.2.1",
+  "com.typesafe.akka" %% "akka-multi-node-testkit" % "2.2.1",
   "org.scalatest" %% "scalatest" % "2.0.M7" % "compile,test"
 )
 
---
+++ ch9-testing-actors/src/multi-jvm/scala/MessageForwardMultiNodeDemo.scala
@@ -0,0 +1,85 @@
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import org.scalatest.matchers.MustMatchers
+import akka.actor._
+import akka.testkit.ImplicitSender
+import akka.remote.testkit._
+
+trait ForwardMultiNodeSpec extends MultiNodeSpecCallbacks
+                    with WordSpecLike
+                    with MustMatchers 
+                    with BeforeAndAfterAll {
+
+    override def beforeAll() = multiNodeSpecBeforeAll()
+
+    override def afterAll() = multiNodeSpecAfterAll()
+}
+
+object MConfig extends MultiNodeConfig {
+    val node1 = role("Actor-1")
+    val node2 = role("Actor-2")
+    val node3 = role("Actor-3")
+}
+
+class ForwardMNMultiJvmNode1 extends ForwardMultipleNodeDemo
+class ForwardMNMultiJvmNode2 extends ForwardMultipleNodeDemo
+class ForwardMNMultiJvmNode3 extends ForwardMultipleNodeDemo
+
+class ForwardMultipleNodeDemo extends MultiNodeSpec(MConfig) 
+                       with ForwardMultiNodeSpec 
+                       with ImplicitSender {
+
+    import MConfig._
+
+    def initialParticipants = roles.size
+
+    override def verifySystemShutdown = true
+
+    override def atStartup() = println("STARTING UP!")
+    override def afterTermination() = println("TERMINATION!")
+
+    "A multi-node illustrating `message-forwarding`" must {
+
+        "wait for all nodes to enter barrier" in {
+
+            enterBarrier("startingup")
+    
+        }
+
+        "send to and receive from a remote node via `forwarding`" in {
+
+            runOn(node1) {
+                enterBarrier("deployed")
+                var condition = false
+                val next = system.actorFor(node(node2) / "user" / "MiddleMan")
+                val me = system.actorOf(Props(new Actor with ActorLogging {
+                    def receive = {
+                        case "hello" ⇒ next forward (self, "hello")
+                        case ("hello",Stop)  ⇒ condition = true
+                    }}), "Initiator")
+
+                me ! "hello"
+                awaitCond(condition == true)
+            }
+
+            runOn(node2) {
+                val next = system.actorFor(node(node3) / "user" / "Destinator")
+                system.actorOf(Props(new Actor with ActorLogging {
+                    def receive = {
+                        case (to, msg) ⇒ next forward (to, msg)
+                    }}), "MiddleMan")
+                enterBarrier("deployed")
+            }
+
+            runOn(node3) {
+                system.actorOf(Props(new Actor with ActorLogging {
+                    def receive = {
+                        case (to:ActorRef, msg) ⇒ to ! (msg, Stop)
+                    }}), "Destinator")
+                enterBarrier("deployed")
+            }
+        
+        enterBarrier("finished")
+    }
+}
+}
+
