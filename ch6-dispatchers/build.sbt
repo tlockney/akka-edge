3334eb1a3d48ce2f7088bce1e86a5bbad2e48d50
Raymond Tay <raymondtay1974@gmail.com>
2013-11-24 11:47:53 +0800

    updated for Akka 2.2.3

--- ch6-dispatchers/build.sbt
+++ ch6-dispatchers/build.sbt
@@ -2,10 +2,10 @@ name := "ch6-dispatchers"
 
 version := "1.0"
 
-scalaVersion := "2.10.1"
+scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
   "org.eclipse.jetty" % "jetty-server" % "9.0.0.v20130308",
   "org.eclipse.jetty" % "jetty-webapp" % "9.0.0.v20130308",
-  "com.typesafe.akka" %% "akka-actor" % "2.1.2"
+  "com.typesafe.akka" %% "akka-actor" % "2.2.3"
 )
--- ch6-dispatchers/src/main/scala/akkaguide/Bookmarker.scala
+++ ch6-dispatchers/src/main/scala/akkaguide/Bookmarker.scala
@@ -3,18 +3,40 @@ package akkaguide
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{ServletHolder, ServletContextHandler}
 import java.util.UUID
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{Props, ActorSystem, ActorRef, Actor}
 import akka.routing.{FromConfig}
+import scala.util.{Try,Success,Failure}
 
 object Bookmarker extends App {
 
+  val helpMessage =
+"""
+$ > sbt "run [dispatchername|actorname|mailboxname]"
+"""
   val system = ActorSystem("bookmarker")
 
   val database = Database.connect[Bookmark, UUID]("bookmarkDatabase")
 
-  val bookmarkStoreGuardian =
+  def startByDispatcherName() : ActorRef = 
     system.actorOf(Props(new BookmarkStoreGuardian(database))
       .withDispatcher("boundedBookmarkDispatcher"))
+
+  def startByActorName() : ActorRef = 
+    system.actorOf(Props(new BookmarkStoreGuardian(database)) ,"boundedMailboxactor")
+
+  def startByMailboxName() : ActorRef = 
+    system.actorOf(Props(new BookmarkStoreGuardian(database)).withMailbox("boundedBookmarkMailbox"))
+
+  val bookmarkStoreGuardian:ActorRef = Try(args(0)) match {
+			                                case Success("dispatchername") ⇒ startByDispatcherName
+			                                case Success("actorname")      ⇒ startByActorName
+			                                case Success("mailboxname")    ⇒ startByMailboxName
+                                            case Success(_)                ⇒ Actor.noSender
+			                                case Failure(e) ⇒ Console.println(helpMessage)
+			                                                  Actor.noSender
+                                        }
+                                                    
+  assert(bookmarkStoreGuardian != Actor.noSender)
 
   val server = new Server(8080)
   val root = new ServletContextHandler(ServletContextHandler.SESSIONS)
